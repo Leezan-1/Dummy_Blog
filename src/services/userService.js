dfe7ec8ecf0ab1e8eac26fb9ff427a6299c19f3d
Leezan <94422693+Leezan-1@users.noreply.github.com>
2025-02-27 14:19:48 +0545

    refactor userservice

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,42 +4,31 @@ const CustomError = require("../utils/CustomError");
 
 class UserService {
     static async signUpUser(userInfo) {
-        console.log('UserService.signUpUser()');
-        try {
-            let userExists = await Users.findOne({ where: { email: userInfo.email } })
+        let userExists = await Users.findOne({ where: { email: userInfo.email } })
 
-            if (userExists)
-                throw new CustomError('User Already Exists', 409);
+        if (userExists)
+            throw new CustomError('User Already Exists', 409);
 
-            let user = Users.build({
-                first_name: userInfo['fname'],
-                last_name: userInfo['lname'],
-                email: userInfo['email'],
-                password: userInfo['password']
-            });
+        let user = Users.build({
+            first_name: userInfo['fname'],
+            last_name: userInfo['lname'],
+            email: userInfo['email'],
+            password: userInfo['password']
+        });
 
-            await user.save();
-            return user;
+        await user.save();
+        return user;
 
-        } catch (error) {
-            throw error;
-        }
     }
 
     static async loginUser(userInfo) {
-        console.log('UserService.loginUser()');
-        try {
+        let userExists = await Users.findOne({ where: { email: userInfo.email } })
 
-            let userExists = await Users.findOne({ where: { email: userInfo.email } })
+        if (!userExists)
+            throw new CustomError('User Not Found', 404);
 
-            if (!userExists)
-                throw new CustomError('User Not Found', 404);
+        return userExists;
 
-            return userExists;
-
-        } catch (error) {
-            throw error;
-        }
     }
 
 
